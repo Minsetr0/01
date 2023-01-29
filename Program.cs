4895db1920b629092bb88c4326cf2e0aa04e83c8
Minsetr0 <nesterovmaksim71@mail.ru>
2023-01-29 23:18:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿Console.WriteLine("Что вы хотите написать?");
 string message = Console.ReadLine();
 Console.WriteLine("Сколько раз?");
-int howManyTimes = Convert.ToInt32(Console.ReadLine());
-for (int i = 0; i < howManyTimes; i++)
+int repeats = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < repeats; i++)
+
 {
     Console.WriteLine(message);
 }
